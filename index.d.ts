ea38592273b5e460b01c2e44e7c5979bd6dd7303
Weidong Fang <weidong@sinoease.com>
2018-06-19 19:05:09 +0930

    Reformat

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,27 @@
 export declare class Response {
   url: string;
   statusCode: number;
-  headers: { [key: string]: string };
+  headers: { [key: string]: string | string[] };
+  httpVersion: string;
+  fetchStart: Date;
+  fetchEnd: Date;
   body: Buffer;
+
+  constructor(url: string, res: any, buf: Buffer);
+
   save(path: string, gzip?: boolean): Promise<Response>;
+
   static load(path: string): Promise<Response>;
 }
 
 export interface RequestOptions {
-  headers: { [key: string]: string | number } | Array<[string, string]>;
-}
-
-export interface UrlRecord {
-  url: string;
+  setHost?: boolean;
+  proxy?: string;
+  timeout?: number;
+  headers?: { [key: string]: string | number } | Array<[string, string]>;
 }
 
 export declare function request(
-  url: string | UrlRecord,
+  url: string,
   options: RequestOptions
 ): Promise<Response>;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
-const URL = require('url')
-const http = require('http')
-const https = require('https')
-const { Response } = require('./src/response')
-const { version } = require('./package.json')
+const URL = require('url');
+const http = require('http');
+const https = require('https');
+const { Response } = require('./src/response');
+const { version } = require('./package.json');
 
 const {
   parseUrl,
@@ -12,33 +12,33 @@ const {
   setHeader,
   setProxy,
   inflate
-} = require('./src/utils')
+} = require('./src/utils');
 
 function request(url, options = {}) {
   return new Promise((resolve, reject) => {
     function _request(url) {
-      const request = buildRequest(parseUrl(url), options)
-
+      const request = buildRequest(parseUrl(url), options);
+      const fetchStart = new Date();
       execute(request, options.data)
         .then(result => {
-          const [res, buf] = result
-          const encoding = res.headers['content-encoding']
-          setCookie(options, res.headers['set-cookie'], url)
-          if (
-            options.inflateContent !== false &&
-            /^gzip|deflate$/.test(encoding)
-          ) {
-            inflate(buf, res.headers['content-encoding']).then(content => {
-              _resolve(new Response(url, res, content))
-            })
+          const [res, buf] = result;
+          const encoding = res.headers['content-encoding'];
+          const inflateContent = options.inflateContent !== false;
+          setCookie(options, res.headers['set-cookie'], url);
+          res.fetchStart = fetchStart;
+          res.fetchEnd = new Date();
+          if (inflateContent && /gzip|deflate/.test(encoding)) {
+            inflate(buf, encoding).then(content => {
+              _resolve(new Response(url, res, content));
+            });
           } else {
-            _resolve(new Response(url, res, buf))
+            _resolve(new Response(url, res, buf));
           }
         })
-        .catch(reject)
+        .catch(reject);
     }
 
-    let redirectCount = 0
+    let redirectCount = 0;
 
     function _resolve(res) {
       if (
@@ -48,139 +48,139 @@ function request(url, options = {}) {
         res.headers.location
       ) {
         if (redirectCount++ >= (options.maxRedirect || 50)) {
-          reject(Error('Too many redirects'))
+          reject(Error('Too many redirects'));
         } else {
-          const nextUrl = URL.parse(URL.resolve(res.url, res.headers.location))
-          redirectCount++
-          _request(nextUrl)
+          const nextUrl = URL.parse(URL.resolve(res.url, res.headers.location));
+          redirectCount++;
+          _request(nextUrl);
         }
       } else {
-        resolve(res)
+        resolve(res);
       }
     }
 
-    _request(url)
-  })
+    _request(url);
+  });
 }
 
 function setCookie(options, cookie, url) {
   if (cookie && typeof options.setCookie === 'function') {
     if (!Array.isArray(cookie)) {
-      options.setCookie(cookie, url)
+      options.setCookie(cookie, url);
     } else {
       cookie.forEach(value => {
-        options.setCookie(value, url)
-      })
+        options.setCookie(value, url);
+      });
     }
   }
 }
 
 function buildRequest(url, options) {
-  const request = pickAssign({}, url, ['protocol', 'hostname', 'path'])
+  const request = pickAssign({}, url, ['protocol', 'hostname', 'path']);
 
   if (url.port !== '') {
-    request.port = parseInt(url.port)
+    request.port = parseInt(url.port);
   }
 
   if (url.username || url.password) {
-    request.auth = `${url.username}:${url.password}`
+    request.auth = `${url.username}:${url.password}`;
   }
 
-  pickAssign(request, options, ['method', 'timeout'])
+  pickAssign(request, options, ['method', 'timeout']);
 
   if (!request.method) {
-    request.method = options.data === undefined ? 'GET' : 'POST'
+    request.method = options.data === undefined ? 'GET' : 'POST';
   }
 
   if (options.headers !== undefined) {
-    request.headers = []
+    request.headers = [];
     if (Array.isArray(options.headers)) {
       for (const header of options.headers) {
-        request.headers.push([...header])
+        request.headers.push([...header]);
       }
     } else {
       for (const key in options.headers) {
-        request.headers.push([key, options.headers[key]])
+        request.headers.push([key, options.headers[key]]);
       }
     }
   } else {
     request.headers = [
-      ['Accept-Encoding', 'gzip, deflate, br'],
+      ['Accept-Encoding', 'gzip, deflate'],
       ['Accept-Language', 'en-AU,en;q=0.9'],
       ['Connection', 'keep-alive'],
       ['User-Agent', `Mozilla/5.0 Yareq/${version}`]
-    ]
+    ];
   }
 
   if (options.setHost === undefined || options.setHost) {
-    setHeader(request.headers, 'Host', url.hostname)
+    setHeader(request.headers, 'Host', url.hostname);
   }
 
   if (options.proxy) {
-    setProxy(request, options.proxy, url)
+    setProxy(request, options.proxy, url);
   }
 
   if (typeof options.getCookie === 'function') {
-    const cookie = options.getCookie(url)
+    const cookie = options.getCookie(url);
     if (cookie) {
-      setHeader(request.headers, 'Cookie', cookie)
+      setHeader(request.headers, 'Cookie', cookie);
     }
   }
 
-  return request
+  return request;
 }
 
 function execute(options, data) {
-  const proto = options.protocol == 'http:' ? http : https
+  const proto = options.protocol == 'http:' ? http : https;
   return new Promise((resolve, reject) => {
     const req = proto.request(options, res => {
-      const chunks = []
+      const chunks = [];
       res.on('data', chunk => {
-        chunks.push(chunk)
-      })
+        chunks.push(chunk);
+      });
 
       res.on('end', () => {
-        resolve([res, Buffer.concat(chunks)])
-      })
-    })
+        resolve([res, Buffer.concat(chunks)]);
+      });
+    });
 
     req.on('socket', socket => {
-      socket.setTimeout(options.timeout || 30000)
+      socket.setTimeout(options.timeout || 30000);
       socket.on('timeout', function() {
-        req.abort()
-        reject(Error('Timeout'))
-      })
-    })
+        req.abort();
+        reject(Error('Timeout'));
+      });
+    });
 
     req.on('error', error => {
       if (!req.aborted) {
-        reject(Error(error))
+        reject(Error(error));
       }
-    })
+    });
 
     if (data !== undefined) {
       if (typeof data.pipe === 'function') {
-        data.pipe(req)
-        return
+        data.pipe(req);
+        return;
       } else {
         if (typeof data === 'object' && !(data instanceof Buffer)) {
-          data = JSON.stringify(data)
+          data = JSON.stringify(data);
         }
-        req.write(data)
+        req.write(data);
       }
     }
 
-    req.end()
-  })
+    req.end();
+  });
 }
 
 module.exports = {
   get: request,
   post: (url, data, options) => {
-    return request(url, Object.assign({}, options, { data }))
+    return request(url, Object.assign({}, options, { data }));
   },
   request,
   Response
-}
+};
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
